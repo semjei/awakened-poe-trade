6dca2238f8b57481ed6f18789f17cd2e8cf258eb
Harry Yu <hy.harry.yu@gmail.com>
2021-10-21 22:20:52 -0700

    Added MacOS support

--- src/main/game-chat.ts
+++ src/main/game-chat.ts
@@ -15,25 +15,31 @@ const AUTO_CLEAR = [
 export function typeInChat (text: string, send: boolean) {
   const saved = clipboard.readText()
 
+  // Modifier keys have to be one of this set:
+  // http://robotjs.io/docs/syntax#keytapkey-modifier
+  // Modifier keys are translated by code here:
+  // https://github.com/octalmage/robotjs/blob/v0.6.0/src/robotjs.cc#L425
+  const modifiers = process.platform === 'darwin' ? ['command'] : ['control'];
+
   if (text.startsWith(PLACEHOLDER_LAST)) {
     text = text.substr(`${PLACEHOLDER_LAST} `.length)
     clipboard.writeText(text)
-    robotjs.keyTap('Enter', ['Ctrl'])
+    robotjs.keyTap('Enter', modifiers)
   } else if (text.endsWith(PLACEHOLDER_LAST)) {
     text = text.slice(0, -PLACEHOLDER_LAST.length)
     clipboard.writeText(text)
-    robotjs.keyTap('Enter', ['Ctrl'])
+    robotjs.keyTap('Enter', modifiers)
     robotjs.keyTap('Home')
     robotjs.keyTap('Delete')
   } else {
     clipboard.writeText(text)
     robotjs.keyTap('Enter')
     if (!AUTO_CLEAR.includes(text[0])) {
-      robotjs.keyTap('A', ['Ctrl'])
+      robotjs.keyTap('A', modifiers)
     }
   }
 
-  robotjs.keyTap('V', ['Ctrl'])
+  robotjs.keyTap('V', modifiers)
 
   if (send) {
     robotjs.keyTap('Enter')
